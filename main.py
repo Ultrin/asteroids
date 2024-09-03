64275256e98c3c7d80d9e407b27537120a97d2da
Ultrin <joegimblett@gmail.com>
2024-09-02 20:18:30 -0400

    added FPS limit to main. Created chrcleshape.py

--- main.py
+++ main.py
@@ -11,21 +11,19 @@ def main():
     pygame.init
 
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, pygame.Color("black"))
+        screen.fill("black")
         pygame.display.flip()
 
-'''
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-'''
-
+        # limit framerate to 60 FPS
+        dt = game_clock.tick(60)/1000
 
 
 
